e98bd103daba1e921489c549e49e8756d0340a70
weiming.chen <weiming.chen@transferwise.com>
2020-07-02 14:52:35 +0800

    update kek component unit test

--- kek/kek_components_test.go
+++ kek/kek_components_test.go
@@ -1,17 +1,108 @@
 package kek
 
-import "testing"
+import (
+	"encoding/hex"
+	"strings"
+	"testing"
+)
 
-func TestAddComponent(t *testing.T) {
-	zmk := NewZMKComponents("visa", 1, 3, "123AB")
+func TestAddComponentInvalidValue(t *testing.T) {
+	kek := NewKEKComponents("visa", 1, 3, "2D617C")
 
-	if zmk == nil {
-		t.Error("Expected non nil zmk component")
+	err := kek.AddComponent(1, "invalid", "DD1376")
+	if err == nil {
+		t.Fatal("should have failed if the component value is invalid")
+	}
+}
+
+func TestAddComponentCheckValueNotTally(t *testing.T) {
+	kek := NewKEKComponents("visa", 1, 3, "2D617C")
+
+	err := kek.AddComponent(1, "E38FD6D9EF85A892F2FBFDD083A407AE", "DD1376")
+	if err == nil {
+		t.Fatal("should have failed if the component check value does not tally")
+	}
+}
+
+func TestAddComponentSuccess(t *testing.T) {
+	kek := NewKEKComponents("visa", 1, 3, "2D617C")
+
+	err := kek.AddComponent(1, "E38FD6D9EF85A892F2FBFDD083A407AE", "DD1375")
+	if err != nil {
+		t.Fatalf("adding component failed with %v", err)
+	}
+}
+
+func TestIsComplete(t *testing.T) {
+	kek := NewKEKComponents("visa", 1, 3, "2D617C")
+	if kek.IsComplete() {
+		t.Fatal("isComplete should report false after 0/3 components have been added")
 	}
 
-	err := zmk.AddComponent(1, "E38FD6D9EF85A892F2FBFDD083A407AE", "DD1375")
+	kek.AddComponent(1, "E38FD6D9EF85A892F2FBFDD083A407AE", "DD1375")
+	if kek.IsComplete() {
+		t.Fatal("isComplete should report false after 1/3 components have been added")
+	}
 
+	kek.AddComponent(2, "D0085DBFFB3723B926CB7980B9EA6268", "DACAF5")
+	if kek.IsComplete() {
+		t.Fatal("isComplete should report false after 2/3 components have been added")
+	}
+
+	kek.AddComponent(3, "20295EBC0B80BF5EF7F78C9125686D3B", "DE5AA9")
+	if !kek.IsComplete() {
+		t.Fatal("isComplete should report false after 3/3 components have been added")
+	}
+}
+
+func TestMergeResultKeyCheckValueDoesNotTally(t *testing.T) {
+	kek := NewKEKComponents("visa", 1, 3, "123AB")
+
+	err := kek.AddComponent(1, "E38FD6D9EF85A892F2FBFDD083A407AE", "DD1375")
 	if err != nil {
-		t.Errorf("Adding components failed with %v", err)
+		t.Fatalf("adding component 1 failed with %v", err)
+	}
+
+	err = kek.AddComponent(2, "D0085DBFFB3723B926CB7980B9EA6268", "DACAF5")
+	if err != nil {
+		t.Fatalf("adding component 2 failed with %v", err)
+	}
+
+	err = kek.AddComponent(2, "20295EBC0B80BF5EF7F78C9125686D3B", "DE5AA9")
+	if err != nil {
+		t.Fatalf("adding component 3 failed with %v", err)
+	}
+
+	_, err = kek.Merge()
+	if err == nil {
+		t.Fatal("should have failed if the result key check value does not tally")
+	}
+}
+
+func TestMergeResultKeySuccess(t *testing.T) {
+	kek := NewKEKComponents("visa", 1, 3, "2D617C")
+
+	err := kek.AddComponent(1, "E38FD6D9EF85A892F2FBFDD083A407AE", "DD1375")
+	if err != nil {
+		t.Fatalf("adding component 1 failed with %v", err)
+	}
+
+	err = kek.AddComponent(2, "D0085DBFFB3723B926CB7980B9EA6268", "DACAF5")
+	if err != nil {
+		t.Fatalf("adding component 2 failed with %v", err)
+	}
+
+	err = kek.AddComponent(3, "20295EBC0B80BF5EF7F78C9125686D3B", "DE5AA9")
+	if err != nil {
+		t.Fatalf("adding component 3 failed with %v", err)
+	}
+
+	resultKey, err := kek.Merge()
+	if err != nil {
+		t.Fatalf("merge result key failed with %v", err)
+	}
+	expectedKey := "13AED5DA1F32347523C708C11F2608FD13AED5DA1F323475"
+	if !strings.EqualFold(expectedKey, hex.EncodeToString(resultKey.KeyBytes)) {
+		t.Fatalf("Expected %s but got back %s", expectedKey, hex.EncodeToString(resultKey.KeyBytes))
 	}
 }
