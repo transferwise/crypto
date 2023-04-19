d8f69732a23210aed3cd68962a4f2fa96d510f01
Lewis Chan <yeouwai.chan@wise.com>
2023-04-19 16:21:26 +0800

    update pgp test key

--- rsa/rsa_factory_test.go
+++ rsa/rsa_factory_test.go
@@ -1,13 +1,15 @@
 /*
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-		http://www.apache.org/licenses/LICENSE-2.0
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 package rsa
 
@@ -16,7 +18,7 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
-	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -78,7 +80,7 @@ func TestEncryptDecrypt(t *testing.T) {
 }
 
 func readPublicKey(filename string) (*rsa.PublicKey, error) {
-	f, err := ioutil.ReadFile(filename)
+	f, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
